3b477723080823dd6fcc4041ad2eca5eed042502
Ben <94129183+benalaluf@users.noreply.github.com>
2024-04-30 15:58:02 +0300

    added security options

--- src/gui/controls/main_view/security/security_control.py
+++ src/gui/controls/main_view/security/security_control.py
@@ -3,22 +3,40 @@ from flet_core import UserControl
 
 from src.gui.Data import global_data
 from src.gui.controls.main_view.security.enable_2fa_control import EnableTwoFA
+from src.gui.controls.main_view.security.security_container import SecurityContainer
 
 
 class SecurityControl(UserControl):
 
     def __init__(self):
         super().__init__()
-        self.body = ft.Container(alignment=ft.alignment.center, expand=True, padding=0)
         self.two_fa_button = ft.ElevatedButton(text="Enable 2FA", width=200)
         self.two_fa_button.on_click = lambda _: self.page.go("/main/security/2fa")
-        self.content = ft.Row(
+
+        self.enable_two_fa = SecurityContainer(
+                "Enable 2FA",
+            "Enable 2FA to secure your account",
+                    ft.Icon(ft.icons.VERIFIED_USER_ROUNDED, color=ft.colors.GREEN, size=50),
+                    lambda _: self.page.go("/main/security/2fa")
+        )
+
+        self.options = ft.Container(
+            ft.Column(
+                controls=[
+                    self.enable_two_fa
+                ],
+                expand=True,
+                spacing=0,
+            ),
+            padding=ft.Padding(50, 0, 50, 0),
+        )
+        self.content = ft.Column(
             [
-                ft.Text("Security"),
-                self.two_fa_button,
-                self.body
+                ft.Text("Security", size=40, weight=ft.FontWeight.BOLD),
+                self.options,
 
             ]
+            , horizontal_alignment=ft.CrossAxisAlignment.CENTER, spacing=50
         )
         self.enable_two_fa = EnableTwoFA()
 
